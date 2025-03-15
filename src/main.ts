8bd0731ab96cbd1ae68f67a92deb43e15c411355
tikhon <tikhonshamruk@gmail.com>
2025-03-15 18:13:43 +0300

    add new features

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api')
+  app.enableCors({
+    origin: ['http://localhost:3001'],
+    credentials: true, 
+    exposedHeaders: 'set-cookie'
+  })
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
